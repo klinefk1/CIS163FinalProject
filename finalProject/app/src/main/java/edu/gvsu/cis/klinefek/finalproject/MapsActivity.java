72e125efbb6c31f84028785c2d1ae76b987c831d
Katherine Klinefelter <ktrumpet@msn.com>
2015-03-19 11:53:04 -0400

    fix map fragment

--- finalProject/app/src/main/java/edu/gvsu/cis/klinefek/finalproject/MapsActivity.java
+++ finalProject/app/src/main/java/edu/gvsu/cis/klinefek/finalproject/MapsActivity.java
@@ -3,6 +3,8 @@ package edu.gvsu.cis.klinefek.finalproject;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
+import android.location.Location;
+import android.location.LocationListener;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,20 +12,28 @@ import android.util.Log;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements
-    GoogleApiClient.ConnectionCallbacks,
-    GoogleApiClient.OnConnectionFailedListener{
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        com.google.android.gms.location.LocationListener{
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     private static final String TAG = "GooglePlayServicesActivity";
 
     private static final String KEY_IN_RESOLUTION = "is_in_resolution";
+
+    private Marker myMarker;
 
     /**
      * Request code for auto Google Play Services error resolution.
@@ -41,61 +51,18 @@ public class MapsActivity extends FragmentActivity implements
      */
     private boolean mIsInResolution;
 
-
+    /**
+     * Called when the activity is starting. Restores the activity state.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        setUpMapIfNeeded();
 
         if (savedInstanceState != null) {
             mIsInResolution = savedInstanceState.getBoolean(KEY_IN_RESOLUTION, false);
         }
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
+        setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
-    }
-
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p/>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p/>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
-        }
-    }
-
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p/>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
-    private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 
 
@@ -114,6 +81,7 @@ public class MapsActivity extends FragmentActivity implements
                     // Optionally, add additional APIs and scopes if required.
                     .addConnectionCallbacks(this)
                     .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
                     .build();
         }
         mGoogleApiClient.connect();
@@ -140,6 +108,7 @@ public class MapsActivity extends FragmentActivity implements
         outState.putBoolean(KEY_IN_RESOLUTION, mIsInResolution);
     }
 
+
     /**
      * Handles Google Play Services resolution callbacks.
      */
@@ -152,7 +121,6 @@ public class MapsActivity extends FragmentActivity implements
                 break;
         }
     }
-
     private void retryConnecting() {
         mIsInResolution = false;
         if (!mGoogleApiClient.isConnecting()) {
@@ -167,6 +135,12 @@ public class MapsActivity extends FragmentActivity implements
     public void onConnected(Bundle connectionHint) {
         Log.i(TAG, "GoogleApiClient connected");
         // TODO: Start making API requests.
+        LocationRequest req = new LocationRequest();
+        req.setInterval(3000); /*every 3 seconds*/
+        req.setFastestInterval(1000); /*how fast our app can handle the notifications */
+        req.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, req, this);
     }
 
     /**
@@ -210,5 +184,63 @@ public class MapsActivity extends FragmentActivity implements
             Log.e(TAG, "Exception while starting resolution activity", e);
             retryConnecting();
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                //setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+//    private void setUpMap() {
+//        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+//    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        LatLng geoPos = new LatLng(location.getLatitude(), location.getLongitude());
+        CameraPosition campos = CameraPosition.builder()
+                .target(geoPos)
+                .zoom(18)
+                .build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(campos));
+        if (myMarker == null) /*if we don't have a marker yet, create and add */
+            myMarker = mMap.addMarker(new MarkerOptions().position(geoPos));
+        else
+            myMarker.setPosition(geoPos);
     }
 }
